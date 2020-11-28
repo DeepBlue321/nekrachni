facae4bee051f09e93c2d8c0bae4aadb2d130c9c
Ondrej Bucek <xbucek3@fi.muni.cz>
2020-11-28 19:38:44 +0100

    ref - routers to seperated file

---
+++ src/Root.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react';
+import  {Switch, Route} from 'react-router'
+import Profile from "./sites/Profile/Profile";
+import Shop from "./sites/Shop/Shop";
+import ProfileSettings from "./sites/ProfileSettings/ProfileSettings";
+import CourseList from "./sites/CourseList/CourseList";
+import WorldList from "./sites/WorldList/WorldList";
+import Home from "./Home/Home";
+
+const RootComponent = function() {
+
+    const [menu, setMenu] = useState(true);
+    return (
+    <div id="app">
+            <Switch>
+                <Route path="/profil">
+                    <Profile level={5} avatar={"src"} points={25} name={"Karel"}/>
+                </Route>
+                <Route path="/obchod">
+                    <Shop money={25} />
+                </Route>
+                <Route path="/nastaveni_profilu">
+                    <ProfileSettings studyType="Ležérní"/>
+                </Route>
+                <Route path="/prehled_kurzu">
+                    <CourseList />
+                </Route>
+                <Route path="/slovnicek_pojmu">
+                    <WorldList />
+                </Route>
+
+                <Route path="/">
+                    <Home setMenu={setMenu}
+                          menu={menu}/>
+                </Route>
+            </Switch>
+        {/*<Toolbar/>
+        <Footer/>
+        <CookieConsent/>*/}
+    </div>);
+};
+
+export default RootComponent
